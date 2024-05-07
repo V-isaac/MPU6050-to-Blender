432d6bd3479a961baf24e082e7f2001bb58d2dae
V-isaac <79458239+V-isaac@users.noreply.github.com>
2024-05-07 14:42:56 +0300

    Added and changed comments

--- scripts/Diploma_test_quaternions.py
+++ scripts/Diploma_test_quaternions.py
@@ -9,9 +9,11 @@ import bpy
 import numpy as np
 import serial 
 
+# Getting script from this exact folder *relatively* to Blender project
 sys.path.append(os.path.join(bpy.path.abspath("//"), "scripts"))
 from uartcommunication import UARTTable
 
+# Setting up logger. If everything works may as well just remove it
 logger = logging.getLogger("BPY")
 logger.setLevel(logging.DEBUG)
 if not logger.handlers:
@@ -21,40 +23,44 @@ if not logger.handlers:
     ch.setFormatter(logging.Formatter("%(asctime)s %(levelname)s [%(name)s]: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"))
     logger.addHandler(ch)
          
-COM_PORT = "COM3"
-FPS = 30 #change for faster/slower communication 24/30/60
-uart_table = UARTTable(COM_PORT, 115200)
+COM_PORT = "COM3" # Communication port (USB). If you're unsure, use Arduino IDE to find out 
+FPS = 30 # Change for slowest/slow/fast communication 24/30/60
+uart_table = UARTTable(COM_PORT, 115200) # Start another script as threaded
 
-#defining functions for operator
-#set positions
+# Defining functions for operator
+# Set positions
 def SetRotations(object, rotation):
     w, x, y, z = rotation
     object.rotation_quaternion[0] = w
     object.rotation_quaternion[1] = x
     object.rotation_quaternion[2] = y
     object.rotation_quaternion[3] = z
-    
+
+# Vector magic    
 def multQuaternion(q1, q0):
     w0, x0, y0, z0 = q0
     w1, x1, y1, z1 = q1
     return np.array([-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
                      x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
                      -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
-                     x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=np.float64) #what the fuck
+                     x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=np.float64)
     
 
-#Тело Оператора
+# Start of an actual script, here we go
 class TestScript(bpy.types.Operator):
+    # Without these Blender will not run an Operator
     bl_idname = "wm.test_script"
     bl_label = "Invoke test script operator"
     
     _timer = None
     
-    #loop
+    # Our loop
     def modal(self, context,event):
-        if event.type in {'RIGHTMOUSE', 'ESC'}: #Отменить
+        if event.type in {'RIGHTMOUSE', 'ESC'}: #Cancel
             logger.info("BlenderTimer received ESC.")
             self.cancel(context)
+            # Alternatively could be used Array of objects
+            # Avoiding it for speed
             SetRotations(bpy.data.objects["Armature"].pose.bones.get("hand.L"), [1, 0, 0, 0])
             SetRotations(bpy.data.objects["Armature"].pose.bones.get("hand.L.B"), [1, 0, 0, 0])
             SetRotations(bpy.data.objects["Armature"].pose.bones.get("hand.L.P"), [1, 0, 0, 0])
@@ -68,23 +74,22 @@ class TestScript(bpy.types.Operator):
             logger.info("Applying transform")
             return {'FINISHED'}
         
-        if event.type == 'TIMER':               #Запускаем Модальный Оператор
-            #Вытаскиваем данные посредством JSON
+        if event.type == 'TIMER':               # Starting Modal Operator with Timer which will return True every 1/FPS 
+            # Getting values via JSON string
             lhand   = uart_table.get("lh0")
             lthumb  = uart_table.get("lh1")
             lpoint  = uart_table.get("lh2")
             lmiddle = uart_table.get("lh3")
             lring   = uart_table.get("lh4")
             lpinky  = uart_table.get("lh5")
-            #logger.debug(str(lhand))
             
+            # If objects do not exist, just pass trough script without doing anything
             if not lhand or not lthumb or not lpoint or not lmiddle or not lring or not lpinky:
-                #logger.debug(str(lhand))
                 logger.warning("Invalid joint data")
                 return{'PASS_THROUGH'}
             
             logger.debug(str(lhand))
-            #перевод в таблицы Numpy
+            # Getting arrays from Strings
             lhand   = np.array(lhand)
             lthumb  = np.array(lthumb)
             lpoint  = np.array(lpoint)
@@ -92,17 +97,17 @@ class TestScript(bpy.types.Operator):
             lring   = np.array(lring)
             lpinky  = np.array(lpinky)
             
-            #инверсия корневой
+            # Inverting root bone
             lhandInv = lhand * np.array([1, -1, -1, -1])
             
-            #получение относительных поворотов
+            # Getting values relatively to root bone
             thumbRel    = multQuaternion(lhandInv, lthumb)
             pointRel    = multQuaternion(lhandInv, lpoint)
             middleRel   = multQuaternion(lhandInv, lmiddle)
             ringRel     = multQuaternion(lhandInv, lring)
             pinkyRel    = multQuaternion(lhandInv, lpinky)
             
-            #присвоить значения с помощью массива Numpy
+            # Set rotations accordingly 
             SetRotations(bpy.data.objects["Armature"].pose.bones.get("hand.L"), lhand)
             SetRotations(bpy.data.objects["Armature"].pose.bones.get("hand.L.B"), thumbRel)
             SetRotations(bpy.data.objects["Armature"].pose.bones.get("hand.L.P"), pointRel)
@@ -110,14 +115,13 @@ class TestScript(bpy.types.Operator):
             SetRotations(bpy.data.objects["Armature"].pose.bones.get("hand.L.R"), ringRel)
             SetRotations(bpy.data.objects["Armature"].pose.bones.get("hand.L.p"), pinkyRel)
                         
-            #без комментария, чтобы форсировать обновление окна
+            # Uncoment if refresh rate is too low 
             #bpy.ops.wm.redraw_timer(type = 'DRAW_WIN_SWAP', iterations = 1)
         return {'RUNNING_MODAL'}
 
     def execute(self, context):
-        # update rate is FPS
-        wm = context.window_manager
-        self._timer = wm.event_timer_add(1./FPS, window=context.window)        
+        # Update rate is FPS
+        self._timer = context.window_manager.event_timer_add(1./FPS, window=context.window)        
         wm.modal_handler_add(self)
         return {'RUNNING_MODAL'}
     
@@ -129,18 +133,15 @@ if __name__ == "__main__":
     try:
         logger.info("Starting services.")
         bpy.utils.register_class(TestScript)
-        
         uart_table.startThreaded()
-
         bpy.ops.wm.test_script()
-        
         logger.info("All started.")
     except KeyboardInterrupt:
         uart_table.stop()
         logger.info("Received KeyboardInterrupt, stopped.")
 
-#uncoment to register in F3 menu    
-#def menu_func(self, context):
+# Uncoment to register in F3 menu    
+#Def menu_func(self, context):
 #    self.layout.operator(TestScript.bl_idname, text="Modal Operator")
 
 #bpy.utils.register_class(TestScript)
